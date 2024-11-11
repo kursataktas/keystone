f58d9ddfb5d8b7f572502edcfc21b42ff27511c8
jossmac <2730833+jossmac@users.noreply.github.com>
2024-11-11 16:42:13 +1100

    critical cosmetic fixes for the document field;
    so it _almost_ fits in with surrounding keystar-ui components

--- packages/fields-document/src/DocumentEditor/primitives/inline-dialog.tsx
+++ packages/fields-document/src/DocumentEditor/primitives/inline-dialog.tsx
@@ -1,6 +1,8 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 
+import { tokenSchema } from '@keystar/ui/style'
+
 import { jsx, Portal, useTheme } from '@keystone-ui/core'
 import { type HTMLAttributes, forwardRef } from 'react'
 
@@ -22,11 +24,12 @@ export const InlineDialog = forwardRef<HTMLDivElement, Props>(({ isRelative, ...
     : {}
 
   const dialog = (
+    
     <div
       ref={ref}
       contentEditable={false}
       css={{
-        background: 'white',
+        background: tokenSchema.color.background.surface,
         borderRadius: radii.small,
         boxShadow: `rgba(9, 30, 66, 0.31) 0px 0px 1px, rgba(9, 30, 66, 0.25) 0px 4px 8px -2px`,
         padding: spacing.small,
--- packages/fields-document/src/views.tsx
+++ packages/fields-document/src/views.tsx
@@ -1,52 +1,38 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
 'use client'
 
-import { jsx } from '@keystone-ui/core'
-import { FieldContainer, FieldDescription, FieldLabel } from '@keystone-ui/fields'
+import { Field as KeystarField } from '@keystar/ui/field'
+import { Text } from '@keystar/ui/typography'
+import type { CellComponent, FieldProps } from '@keystone-6/core/types'
+import React from 'react'
 import { Node } from 'slate'
-
-import {
-  type CellComponent,
-  type FieldProps,
-} from '@keystone-6/core/types'
 
 import { DocumentEditor } from './DocumentEditor'
 import { ForceValidationProvider } from './DocumentEditor/utils-hooks'
-import {
-  type controller,
-  type DocumentFeatures,
-} from './views-shared'
+import { type controller, type DocumentFeatures } from './views-shared'
 
-export {
-  controller,
-} from './views-shared'
-
+export { controller } from './views-shared'
 export { type DocumentFeatures }
-export function Field ({
-  field,
-  value,
-  onChange,
-  autoFocus,
-  forceValidation,
-}: FieldProps<typeof controller>) {
-  return <FieldContainer>
-    <FieldLabel as="span" id={`${field.path}-label`}>
-      {field.label}
-    </FieldLabel>
-    <FieldDescription id={`${field.path}-description`}>{field.description}</FieldDescription>
-    <ForceValidationProvider value={!!forceValidation}>
-      <DocumentEditor
-        autoFocus={autoFocus}
-        aria-labelledby={`${field.path}-label`}
-        value={value}
-        onChange={onChange}
-        componentBlocks={field.componentBlocks}
-        relationships={field.relationships}
-        documentFeatures={field.documentFeatures}
-      />
-    </ForceValidationProvider>
-  </FieldContainer>
+
+export function Field (props: FieldProps<typeof controller>) {
+  const { autoFocus, field, forceValidation, onChange, value } = props
+
+  return (
+    <KeystarField label={field.label} description={field.description}>
+      {inputProps => (
+        <ForceValidationProvider value={!!forceValidation}>
+          <DocumentEditor
+            {...inputProps}
+            autoFocus={autoFocus}
+            componentBlocks={field.componentBlocks}
+            documentFeatures={field.documentFeatures}
+            onChange={onChange}
+            relationships={field.relationships}
+            value={value}
+          />
+        </ForceValidationProvider>
+      )}
+    </KeystarField>
+  )
 }
 
 function serialize (nodes: Node[]) {
@@ -57,9 +43,7 @@ export const Cell: CellComponent = ({ field, item }) => {
   const value = item[field.path]?.document
   if (!value) return null
 
-  // FIXME: @keystar/ui `Text` should be used here, but it's not currently
-  // available in this package
-  return <span>{serialize(value).slice(0, 60)}</span>
+  return <Text>{serialize(value).slice(0, 60)}</Text>
 }
 
 export const allowedExportsOnCustomViews = ['componentBlocks']
